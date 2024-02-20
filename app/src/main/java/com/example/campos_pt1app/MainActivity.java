c76931ba92fce01a97473351cf980278be3f37aa
KiaraBiancaCampos <kbcmps22@gmail.com>
2024-02-20 13:59:30 +0800

    my app version1 - int number = 1;

---
+++ app/src/main/java/com/example/campos_pt1app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.campos_pt1app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int number = 1;
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CAMPOS_PT1APP"
+include(":app")
+ 
